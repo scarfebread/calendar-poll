be38131d2ee49f9e4fb8c6817856ba7b7d277558
James Scarfe <james.scarfe@live.co.uk>
2023-01-20 09:46:38 +0000

    Corrected Kafka properties path

--- src/jvmMain/kotlin/event/KafkaProperties.kt
+++ src/jvmMain/kotlin/event/KafkaProperties.kt
@@ -8,7 +8,7 @@ import java.util.*
 
 val kafkaConfig: Properties
     get() = Properties().apply {
-        load(FileInputStream("/home/scarfebread/code/calendar-poll/src/jvmMain/resources/kafka.properties"))
+        load(FileInputStream("src/jvmMain/resources/kafka.properties"))
         this[ProducerConfig.ACKS_CONFIG] = "all"
         this[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName
         this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaJsonSerializer::class.qualifiedName
